0f31c52a641f5cffb9fef683a4a96b9e5ee08dfa
lapin <lapin@kontur.ru>
2018-03-23 17:57:53 +0400

    first commit

---
+++ BuildStatus/AppveyorStatus.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+
+namespace BuildStatus
+{
+    public class AppveyorStatus
+    {
+        private static readonly string apiToken = Environment.GetEnvironmentVariable("APPVEYOR_TOKEN");
+        private static readonly HttpClient httpClient = new HttpClient();
+
+        static AppveyorStatus()
+        {
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiToken);
+        }
+
+        [Test, TestCaseSource(nameof(ProjectObjects))]
+        public void AppveyorProjectStatus(string projectName)
+        {
+            using (var response = httpClient.GetAsync($"https://ci.appveyor.com/api/projects/vostok/{projectName}/history?recordsNumber=10&branch=master").Result)
+            {
+                response.EnsureSuccessStatusCode();
+                var json = response.Content.ReadAsStringAsync().Result;
+                var history = JToken.Parse(json);
+
+                var lastBuild = ((JArray)history["builds"]).FirstOrDefault(b => (string)b["status"] != "cancelled" && (string)b["status"] != "running");
+                if (lastBuild == null)
+                    return;
+                var status = (string)lastBuild["status"];
+                if (status != "success")
+                {
+                    throw new Exception("build failed with status " + status);
+                }
+            }
+            //Console.WriteLine(role.Value<string>("name"));
+        }
+
+        private static IEnumerable<object[]> ProjectObjects()
+        {
+            // get the list of roles
+            using (var response = httpClient.GetAsync("https://ci.appveyor.com/api/projects").Result)
+            {
+                response.EnsureSuccessStatusCode();
+
+                var json = response.Content.ReadAsStringAsync().Result;
+                var projects = JArray.Parse(json);
+                foreach (var project in projects)
+                {
+                    yield return new object[] { (string)project["slug"] };
+                }
+            }
+        }
+    }
+}
---
+++ BuildStatus/DockerStatus.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+
+namespace BuildStatus
+{
+    [TestFixture]
+    public class DockerStatus
+    {
+        private static readonly WebClient webClient = new WebClient();
+        //https://hub.docker.com/v2/repositories/vstk/airlock.consumer/buildhistory/?page_size=10
+        //https://hub.docker.com/v2/repositories/vstk/
+
+        [TestCaseSource(nameof(ProjectNames))]
+        public void DockerProjectStatus(string projectName)
+        {
+            var json = webClient.DownloadString(
+                $"https://hub.docker.com/v2/repositories/vstk/{projectName}/buildhistory/?page_size=100");
+            dynamic jObject = JObject.Parse(json);
+            if (jObject.count == 0)
+                throw new Exception("build count is 0");
+            //dockertag_name
+            var masterLastBuild = ((JArray)jObject.results).FirstOrDefault(x => (string)x["dockertag_name"] == "latest");
+            if (masterLastBuild == null)
+                throw new Exception("master build not found");
+            int status = (int)masterLastBuild["status"];
+            if (status < 0)
+                throw new Exception("build failed");
+        }
+
+
+        private static IEnumerable<object[]> ProjectNames()
+        {
+            var json = webClient.DownloadString($"https://hub.docker.com/v2/repositories/vstk/");
+            dynamic jObject = JObject.Parse(json);
+            int count = jObject.count;
+            if (count == 0)
+                throw new Exception("project count is 0");
+            foreach (var projObj in ((JArray)jObject.results))
+            {
+                yield return new object[] { (string)projObj["name"] };
+            }
+        }
+    }
+}
